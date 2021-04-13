ca452a795279000363b69652167bc15e978e7ed0
dhanasekaran <dhanasekaran164@gmail.com>
2021-04-13 19:10:53 +0530

    commanded bulk scrap code and enable single date input basis scrap

--- app.py
+++ app.py
@@ -51,52 +51,52 @@ def test():
 		temp["data"] = []
 		temp["error"] = "date must be previous date from current date"
 		return render_template('index.html', data=temp)
-	sdate = date(2021, 1, 1)
-	edate = date(2021, 4, 13)
+	# sdate = date(2021, 1, 1)
+	# edate = date(2021, 4, 13)
 
-	delta = edate - sdate
+	# delta = edate - sdate
 
-	for i in range(delta.days + 1):
-		day = sdate + timedelta(days=i)
-		_cur_date = datetime.strftime(day, '%d/%m/%Y')
-		_cur_date_obj_ = datetime.strptime(_cur_date, '%d/%m/%Y')
-		_formated_date_ = _cur_date_obj_.strftime('%Y-%m-%d')
+	# for i in range(delta.days + 1):
+	# 	day = sdate + timedelta(days=i)
+	# 	_cur_date = datetime.strftime(day, '%d/%m/%Y')
+	# 	_cur_date_obj_ = datetime.strptime(_cur_date, '%d/%m/%Y')
+	# 	_formated_date_ = _cur_date_obj_.strftime('%Y-%m-%d')
 
-		driver = webdriver.Chrome(executable_path=r"/home/dhanasekaran/dhana/practice/metro/chromedriver")
-		driver.get('http://123.63.203.150/reserve3.asp');
-		time.sleep(5)
-		driver.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/font/input[1]").send_keys(_cur_date)
-		driver.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/font/input[2]").click()
-		time.sleep(3)
-		
-		total_rows 		= 12
-		total_columns 	= 10
+	driver = webdriver.Chrome(executable_path=r"/home/dhanasekaran/dhana/practice/metro/chromedriver")
+	driver.get('http://123.63.203.150/reserve3.asp');
+	time.sleep(5)
+	driver.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/font/input[1]").send_keys(_cur_date)
+	driver.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td/font/input[2]").click()
+	time.sleep(3)
+	
+	total_rows 		= 12
+	total_columns 	= 10
 
-		datas 		= []
+	datas 		= []
 
-		for i in range(2, total_rows):
-			temp = {}
-			for j in range(1, total_columns):
-				text = driver.find_element_by_xpath("/html/body/form/center[2]/table/tbody[2]/tr[{first}]/td[{second}]".format(first=i, second=j)).text
-				temp[j] = text
-			datas.append(temp)
-		
-		for item in datas:
-			sql = "select count(*) as total from `sea_level` where `entry_date` = '{date}' and reservoir = '{res}'".format(date=_formated_date_, res=item[1])
-			cursor = connection.cursor()
+	for i in range(2, total_rows):
+		temp = {}
+		for j in range(1, total_columns):
+			text = driver.find_element_by_xpath("/html/body/form/center[2]/table/tbody[2]/tr[{first}]/td[{second}]".format(first=i, second=j)).text
+			temp[j] = text
+		datas.append(temp)
+	
+	for item in datas:
+		sql = "select count(*) as total from `sea_level` where `entry_date` = '{date}' and reservoir = '{res}'".format(date=_formated_date_, res=item[1])
+		cursor = connection.cursor()
+		cursor.execute(sql)
+		result = cursor.fetchone()
+		if result["total"] > 0:
+			#update stuff
+			sql = "update `sea_level` set `entry_date` = '{entry_date}',`full_tank` = '{full_tank}',`full_capacity`='{full_capacity}',`level`='{level}',`storage`='{storage}',`inflow`='{inflow}',`outflow`='{outflow}',`rainfall`='{rainfall}',`last_year_storage`='{last_year_storage}' where `reservoir` = '{reservoir}'".format(entry_date=_formated_date_,full_tank=item[2],full_capacity=item[3],level=item[4],storage=item[5],inflow=item[6],outflow=item[7],rainfall=item[8],last_year_storage=item[9], reservoir=item[1])
 			cursor.execute(sql)
-			result = cursor.fetchone()
-			if result["total"] > 0:
-				#update stuff
-				sql = "update `sea_level` set `entry_date` = '{entry_date}',`full_tank` = '{full_tank}',`full_capacity`='{full_capacity}',`level`='{level}',`storage`='{storage}',`inflow`='{inflow}',`outflow`='{outflow}',`rainfall`='{rainfall}',`last_year_storage`='{last_year_storage}' where `reservoir` = '{reservoir}'".format(entry_date=_formated_date_,full_tank=item[2],full_capacity=item[3],level=item[4],storage=item[5],inflow=item[6],outflow=item[7],rainfall=item[8],last_year_storage=item[9], reservoir=item[1])
-				cursor.execute(sql)
-				connection.commit()
-			else:
-				#insert stuff
-				sql = "insert into `sea_level` set `entry_date` = '{entry_date}',`reservoir` = '{reservoir}',`full_tank` = '{full_tank}',`full_capacity`='{full_capacity}',`level`='{level}',`storage`='{storage}',`inflow`='{inflow}',`outflow`='{outflow}',`rainfall`='{rainfall}',`last_year_storage`='{last_year_storage}'".format(entry_date=_formated_date_,reservoir=item[1],full_tank=item[2],full_capacity=item[3],level=item[4],storage=item[5],inflow=item[6],outflow=item[7],rainfall=item[8],last_year_storage=item[9])
-				
-				cursor.execute(sql)
-				connection.commit()
+			connection.commit()
+		else:
+			#insert stuff
+			sql = "insert into `sea_level` set `entry_date` = '{entry_date}',`reservoir` = '{reservoir}',`full_tank` = '{full_tank}',`full_capacity`='{full_capacity}',`level`='{level}',`storage`='{storage}',`inflow`='{inflow}',`outflow`='{outflow}',`rainfall`='{rainfall}',`last_year_storage`='{last_year_storage}'".format(entry_date=_formated_date_,reservoir=item[1],full_tank=item[2],full_capacity=item[3],level=item[4],storage=item[5],inflow=item[6],outflow=item[7],rainfall=item[8],last_year_storage=item[9])
+			
+			cursor.execute(sql)
+			connection.commit()
 
 
 
